18df7be5e5dfbe1c40e60fff2e843988e75aaeb9
ganeshsoni-9 <https://github.com/ganeshsoni-9>
2026-07-15 12:11:18 +0530

    add.

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "http://192.168.31.233:5173"
+    "http://192.168.31.233:5173",
+    process.env.CLIENT_URL
   ],
   credentials: true,
 }));
